7c6ea65750a215a411a6e83271fb731130e948a8
dengj <623218869@qq.com>
2022-09-02 11:13:57 +0000

    add cmdline for tradeclient-ccapi using CLI11

--- cxx/tradeclient-ccapi/main.cpp
+++ cxx/tradeclient-ccapi/main.cpp
@@ -16,20 +16,49 @@ void fromAppCallback(std::string message, std::string sessionId)
 
 int main( int argc, char** argv )
 {
-    if ( argc < 2 )
-    {
-        std::cout << "usage: " << argv[ 0 ]
-                  << " FILE." << std::endl;
-        return 0;
-    }
-    std::string file = argv[ 1 ];
+    std::vector<std::string> coinpairs;
+    std::string exchangeName;
+
+    std::string symbol;
+    char side;
+    std::string configfile = "";
+    int interval = 60;
+    double quantity = 0.0;
+    char time_in_force;
+    double price;
+
+    CLI::App app{"app: tradeclient-ccapi"};
+    CLI::App* subcom = app.add_subcommand("test", "description");
+                         
+    app.add_option("-e,--exchane", exchangeName, "exchane name");
+    // app.add_option("-c,--coin", coinpairs, "coinpairs, -c [BTC-ETH, BTC-USDT]");
+    
+    app.add_option("-s,--symbol", symbol, "symbol, -s BTCUSDT");
+    app.add_option("-s,--side", side, "side, -s buy|sell");
+    app.add_option("-q,--quantity", quantity, "quantity, -q 1.22");
+    app.add_option("-p,--price", price, "price, -p 1.22");
+    app.add_option("-t,--time_in_force", time_in_force, "time_in_force, -q 1.22");
+    
+
+    app.add_option("-q,--configfile", configfile, "-f ./configfile");
+
+    CLI11_PARSE(app, argc, argv);
+    // if ( argc < 2 )
+    // {
+    //     std::cout << "usage: " << argv[ 0 ]
+    //               << " FILE." << std::endl;
+    //     return 0;
+    // }
+    // std::string file = argv[ 1 ];
 
     try
     {
 
-        auto clientApifiny = create_client(TradeClientType_Apifiny, file, fromAppCallback);
+        auto clientApifiny = create_client(TradeClientType_CCApi, configfile, fromAppCallback);
         clientApifiny->start();
-//        clientApifiny->put_order();
+        clientApifiny->put_order(symbol, side,
+                                 quantity, price,
+                                 time_in_force);
         clientApifiny->stop();
 
         clientApifiny.release();
